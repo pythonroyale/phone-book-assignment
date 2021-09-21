b31b20e315fe94a2fd2122c048bee0fc554e5a32
pythonroyale <90867253+pythonroyale@users.noreply.github.com>
2021-09-20 20:26:47 -0600

    Add files via upload

--- PhoneBook_Contact_info.py
+++ PhoneBook_Contact_info.py
@@ -1,1 +1,20 @@
-a = 200
+
+
+name1 = ( """ The name: Mr. Cop, The addriss is: 434 Edmonton police court, 
+phone number: 911 """ )
+
+
+name2 = ( """ The name: Pizza 73, The addriss is: 3520 137 Ave NW,
+phone number: 7804737373 """ )
+
+
+name3 = ( """ The name: Uber, The addriss is: 7878 Ave NE, 
+phone number: 857586758 """ )
+
+
+name4 = ( """ The name: clearviwe, The addriss 8576 Ave NW,
+phone number: 758567586 """ )
+
+
+name5 = ( """ The name: wallmart, The addriss 5475 Ave NN,
+phone number: 8594756947 """ )
--- PhoneBook_main_Module.py
+++ PhoneBook_main_Module.py
@@ -1,1 +1,40 @@
-# Hello
+
+
+# In this file I am going to import moudules from my PhoneBook_Contact_info. 
+print ("")
+print ( """ Hello there, I am your digtal phonebook.
+Go ahead and enter who you are looking for. """ )
+# Here I just imported the module
+import PhoneBook_Contact_info
+# In this section I just imported the varibles (name1) (name2) (name3) (name4) (name5)
+policeofficer = PhoneBook_Contact_info.name1
+Pizza73 = PhoneBook_Contact_info.name2
+Uber = PhoneBook_Contact_info.name3
+# In this code I am using the (input) statement 
+find = ( input ( " Who are you looking for sir ? " ))
+# And here I am using the (if) statement so the code can give you results as you ask for it
+if find == "policeofficer":
+    print ( " Here is your contact info that you asked for: " , policeofficer )
+# Finally the last code is a (else) statement to give a massege if what you looked for does not exist
+else:
+    print ( " That contact does not exist sorry ! " )
+
+ 
+find = ( input ( " Who else are you looking for sir ? " ))
+
+if find == "Pizza73":
+    print ( " Here is what you asked for: " , Pizza73 )
+
+else:
+    print ( " That contact does not exist sorry ! " )
+
+
+find = ( input ( " Who else are you looking for sir ? " ))
+
+if find == "Uber":
+    print ( " Here are the results: " , Uber)
+
+else:
+    print ( " sorry we didn't find anything ! " )
+
+
