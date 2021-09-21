7be85a15fb1667239efc25ee07cfa0d6ea7ecc0f
pythonroyale <90867253+pythonroyale@users.noreply.github.com>
2021-09-21 13:18:40 -0600

    Add files via upload

--- PhoneBook_Contact_info.py
+++ PhoneBook_Contact_info.py
@@ -1,20 +1,16 @@
 
+# This is the veribles.
 
-name1 = ( """ The name: Mr. Cop, The addriss is: 434 Edmonton police court, 
-phone number: 911 """ )
-
-
-name2 = ( """ The name: Pizza 73, The addriss is: 3520 137 Ave NW,
-phone number: 7804737373 """ )
+name1 = ( """ The name: Mr. Cop, The addriss is: 434 Edmonton police court, phone number: 911 """ )
 
 
-name3 = ( """ The name: Uber, The addriss is: 7878 Ave NE, 
-phone number: 857586758 """ )
+name2 = ( """ The name: Pizza 73, The addriss is: 3520 137 Ave NW, phone number: 7804737373 """ )
 
 
-name4 = ( """ The name: clearviwe, The addriss 8576 Ave NW,
-phone number: 758567586 """ )
+name3 = ( """ The name: Uber, The addriss is: 7878 Ave NE, phone number: 857586758 """ )
 
 
-name5 = ( """ The name: wallmart, The addriss 5475 Ave NN,
-phone number: 8594756947 """ )
+name4 = ( """ The name: clearviwe, The addriss 8576 Ave NW, phone number: 758567586 """ )
+
+
+name5 = ( """ The name: wallmart, The addriss 5475 Ave NN, phone number: 8594756947 """ )
--- PhoneBook_main_Module.py
+++ PhoneBook_main_Module.py
@@ -1,25 +1,37 @@
 
 
 # In this file I am going to import moudules from my PhoneBook_Contact_info. 
+
 print ("")
-print ( """ Hello there, I am your digtal phonebook.
-Go ahead and enter who you are looking for. """ )
+
+print ( """ Hello there, I am your digtal phonebook. Go ahead and enter who you are looking for. """ )
+
 # Here I just imported the module
+
 import PhoneBook_Contact_info
-# In this section I just imported the varibles (name1) (name2) (name3) (name4) (name5)
+
+# In this section I just imported the varibles from my INFO PHONE BOOK FILE (name1) (name2) (name3) (name4) (name5)
+
 policeofficer = PhoneBook_Contact_info.name1
 Pizza73 = PhoneBook_Contact_info.name2
 Uber = PhoneBook_Contact_info.name3
-# In this code I am using the (input) statement 
+Clearviwe = PhoneBook_Contact_info.name4
+Walmart = PhoneBook_Contact_info.name5
+
+# In this code I am using the (input) statement.
+ 
 find = ( input ( " Who are you looking for sir ? " ))
-# And here I am using the (if) statement so the code can give you results as you ask for it
+
+# And here I am using the (if) statement so the code can give you results as you ask for it.
+
 if find == "policeofficer":
     print ( " Here is your contact info that you asked for: " , policeofficer )
-# Finally the last code is a (else) statement to give a massege if what you looked for does not exist
+
+# Finally the last code is a (else) statement to give a massege if what you looked for does not exist.
+
 else:
     print ( " That contact does not exist sorry ! " )
 
- 
 find = ( input ( " Who else are you looking for sir ? " ))
 
 if find == "Pizza73":
@@ -28,7 +40,6 @@ if find == "Pizza73":
 else:
     print ( " That contact does not exist sorry ! " )
 
-
 find = ( input ( " Who else are you looking for sir ? " ))
 
 if find == "Uber":
@@ -36,5 +47,28 @@ if find == "Uber":
 
 else:
     print ( " sorry we didn't find anything ! " )
+
+
+find = ( input ( " Who else are you looking for ? " ))
+
+if find == "Clearviwe":
+    print ( " Here are the results: " , Clearviwe)
+
+else:
+    print ( " SORRY NO RESULTS ! " )
+
+find = ( input ( " you have reached your limits for looking up contact information in our digital phone book, this is the last lookup for you ! " ))
+
+if find == "Walmart":
+    print ( " Here you go: " , Walmart)
+    
+else:
+    print ( " No results found ! " )
+
+
+
+
+
+
 
 
